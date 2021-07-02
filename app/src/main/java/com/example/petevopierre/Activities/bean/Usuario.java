d3e3667002b845de302af870c49502ba945d420d
Victor Pierre de Lima Guimarães <victorpierre155@gmail.com>
2021-07-02 16:44:11 -0300

    second

--- app/src/main/java/com/example/petevopierre/Activities/bean/Usuario.java
+++ app/src/main/java/com/example/petevopierre/Activities/bean/Usuario.java
@@ -3,11 +3,15 @@ package com.example.petevopierre.Activities.bean;
 import java.io.Serializable;
 
 import mobi.stos.podataka_lib.annotations.Column;
+import mobi.stos.podataka_lib.annotations.ForeignKey;
 import mobi.stos.podataka_lib.annotations.PrimaryKey;
 
 public class Usuario implements Serializable {
     @PrimaryKey(autoIncrement = false)
     private int id;
+
+    @Column
+    private int idloja;
 
     @Column
     private String email;
@@ -26,5 +30,13 @@ public class Usuario implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getIdloja() {
+        return idloja;
+    }
+
+    public void setIdloja(int idloja) {
+        this.idloja = idloja;
     }
 }
